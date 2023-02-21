3262694ef74e5517ce2a793f6214207bbc9433e7
SC0d3r <sasan.kelishani@gmail.com>
2023-02-21 17:24:47 +0330

    add tests for tokenizeIban, turnIbanIntoBban

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 const { turnBbanIntoIban, BANK_CODES, getBankNameFromIban, isBankCodeValid, formatIban } = require("..")
 const isIbanValid = require("../src/isIbanValid")
+const tokenizeIban = require("../src/tokenizeIban")
+const turnIbanIntoBban = require("../src/turnIbanIntoBban")
 
 function test() {
   const iban = "IR870570028180010653892101"
@@ -8,6 +10,13 @@ function test() {
   const bban = "4695069893"
   const calculatedIban = turnBbanIntoIban(bban, BANK_CODES.TEJARAT)
   console.log("funciton turnBbanIntoIban:", "[input] bban", bban, "[input] bankCode BANK_CODES.TEJARAT", "[result] iban", calculatedIban)
+
+  const calculatedBban = turnIbanIntoBban(iban)
+  console.log("function turnIbanIntoBban", "[input] iban", iban, "[result]", calculatedBban)
+
+
+  const tokenizedIban = tokenizeIban(iban)
+  console.log("function tokenizedIban:", "[input] iban", iban, "[result]", tokenizedIban)
 
   console.log("function isIBanValid:", "input [calculateIban]", calculatedIban, "[result]", isIbanValid(calculatedIban))
 
